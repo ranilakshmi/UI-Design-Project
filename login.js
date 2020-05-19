d0396708c9f0228ecb6f25f7218cf0301ceedbab
ranilakshmi <63279825+ranilakshmi@users.noreply.github.com>
2020-05-19 20:07:13 +0530

    Update login.js

--- login.js
+++ login.js
@@ -3,11 +3,14 @@ function validate()
     var username =document.getElementById("email").value;
     var password = document.getElementById("pwd").value;
     
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    
     if (username==""||password==""){
         alert("Fields should not be empty");    
     }
-    else if ('@' not in username){
-        alert("Enter a email address as username")
+    
+    else if(inputText.value.match(mailformat)==False){
+        alert("Enter a email address as username");
     }
     else{
         alert("All inputs are valid");
