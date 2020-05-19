3ccfaabfdc382b7a116a77d6c266a391d118c63d
ranilakshmi <63279825+ranilakshmi@users.noreply.github.com>
2020-05-19 19:54:38 +0530

    Update login.js

--- login.js
+++ login.js
@@ -6,6 +6,9 @@ function validate()
     if (username==""||password==""){
         alert("Fields should not be empty");    
     }
+    else if ('@' not in username){
+        alert("Enter a email address as username")
+    }
     else{
         alert("All inputs are valid");
     }
