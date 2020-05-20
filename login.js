2562d8cdfa12abedef088289c133dac481dba2d6
ranilakshmi <63279825+ranilakshmi@users.noreply.github.com>
2020-05-20 17:13:58 +0530

    Add files via upload

--- login.js
+++ login.js
@@ -3,25 +3,37 @@ function validate()
     var username =document.getElementById("email").value;
     var password = document.getElementById("pwd").value;
     
-    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    
+    var atposition=username.indexOf("@");  
+    var dotposition=username.lastIndexOf("."); 
     if (username==""||password==""){
         alert("Fields should not be empty");    
-    }
-    
-    else if(ValidateEmail(username)==False){
-        alert("You have entered an invalid email address!");
+    } 
+    else if (atposition<1 || dotposition<atposition+2 || dotposition+2>=username.length){  
+        alert("Please enter a valid e-mail address \n atpostion:"+atposition+"\n dotposition:"+dotposition);  
     }
     else{
         alert("All inputs are valid");
     }
-}
-function ValidateEmail(mail) 
-{
- if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(username.value))
-  {
-    return (true)
-  }
-    
-    return (false)
-}
+}  
+
+function register(){
+    var username=document.getElementById("email").value;
+    var password =document.getElementById("pwd").value;
+    var password1 = document.getElementById("pwd1").value;
+
+    var atposition=username.indexOf("@");  
+    var dotposition=username.lastIndexOf("."); 
+
+    if (username==""||password==""){
+        alert("Fields should not be empty");    
+    }
+    else if (atposition<1 || dotposition<atposition+2 || dotposition+2>=username.length){  
+        alert("Please enter a valid e-mail address \n atpostion:"+atposition+"\n dotposition:"+dotposition);  
+    }
+    else if (password!==password1){
+        alert("Password doesn't match");
+    }
+    else{
+        alert("All inputs are valid");
+    }
+}  
