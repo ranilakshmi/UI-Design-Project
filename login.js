652b3b252ab682f7eb0ffb3acbe3d929b9e186ed
ranilakshmi <63279825+ranilakshmi@users.noreply.github.com>
2020-05-21 14:56:17 +0530

    Add files via upload

--- login.js
+++ login.js
@@ -37,3 +37,35 @@ function register(){
         alert("All inputs are valid");
     }
 }  
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('clock').innerHTML =
+    h + ":" + m + ":" + s;
+    var t = setTimeout(startTime, 500);
+  }
+  function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+  }
+  
+
+  var i=0;
+    var text = new Array();
+    text[0]="There is only one thing that makes a dream impossible to achieve: the fear of failure.";
+    text[1]="Every blessing ignored becomes a curse.";
+    text[2]="Nothing in the world is ever completely wrong. Even a stopped clock is right twice a day.";
+ function quotes(){
+    document.getElementById("quote").innerHTML=text[i];
+    if (i<path.length-1) {
+        i++;}
+    else {i=0;}}
+        setTimeout("quotes()",1000);
+ function main(){
+     quotes();
+     startTime();
+ }
